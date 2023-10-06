4b4f4080781160d258e8e68f15dac556486157aa
Daniel Gomes <138718021+DanielGomesSantos@users.noreply.github.com>
2023-10-06 15:55:07 -0300

    formato dia 2
    Atualizações da  interface dia 2

--- interface.py
+++ interface.py
@@ -1,18 +1,29 @@
 import customtkinter
 
+
 janela = customtkinter.CTk()
-janela.geometry("500x300")
+janela.geometry("800x600")
+janela.title("Sistema  de pergunda da Samsung")
+customtkinter.set_appearance_mode("dark")
+
 
 def clique():
     print("Enviar")
 
-texto = customtkinter.CTkLabel(janela, text="Tire suas dúvidas")
-texto.pack(padx=10, pady=80)
 
-pergunta = customtkinter.CTkEntry(janela, placeholder_text= "Pergunta:")
+
+texto = customtkinter.CTkLabel(janela, text="Tire suas dúvidas", font=('arial bold', 30,))
+texto.pack(padx=10, pady=40)
+
+pergunta = customtkinter.CTkEntry(janela, placeholder_text= "Pergunta:", width=250, height=45)
 pergunta.pack(padx=10, pady=10)
 
-botao = customtkinter.CTkButton(janela, text="Enviar", command=clique) 
+
+botao = customtkinter.CTkButton(janela, text="Enviar", command=clique,corner_radius=32, fg_color='#522fdf', width=20, height=20) 
 botao.pack(padx=10, pady=10)
+
+resposta = customtkinter.CTkEntry(janela, placeholder_text= "resposta:", width=250, height=45)
+resposta.pack(padx=10, pady=10)
+
 
 janela.mainloop()
