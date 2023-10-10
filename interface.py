a016192deaf8a415dfa9fa3b84a9d0f64142a799
Daniel Gomes <138718021+DanielGomesSantos@users.noreply.github.com>
2023-10-10 19:02:41 -0300

    terceira atualização da interface

--- interface.py
+++ interface.py
@@ -1,29 +1,30 @@
+from customtkinter import *
 import customtkinter
 
 
-janela = customtkinter.CTk()
-janela.geometry("800x600")
-janela.title("Sistema  de pergunda da Samsung")
+app = CTk()
+app.title("Chatbot Samsung")
+app.geometry("800x600")
 customtkinter.set_appearance_mode("dark")
 
+texto = customtkinter.CTkLabel(master=app, text="Tire suas dúvidas", font=('Castellar', 25,), text_color=('#522fdf'))
+texto.pack(padx=10, pady=40)
+
+pergunta = CTkTextbox(master=app, scrollbar_button_hover_color='#522fdf', scrollbar_button_color="#a4e0cd", corner_radius=16, border_color="#a4e0cd", border_width=1)
+pergunta.pack(padx=10, pady=5,)
 
 def clique():
     print("Enviar")
 
 
-
-texto = customtkinter.CTkLabel(janela, text="Tire suas dúvidas", font=('arial bold', 30,))
-texto.pack(padx=10, pady=40)
-
-pergunta = customtkinter.CTkEntry(janela, placeholder_text= "Pergunta:", width=250, height=45)
-pergunta.pack(padx=10, pady=10)
-
-
-botao = customtkinter.CTkButton(janela, text="Enviar", command=clique,corner_radius=32, fg_color='#522fdf', width=20, height=20) 
+botao = customtkinter.CTkButton(master=app, text="Enviar", command=clique,corner_radius=32, fg_color='#522fdf', width=20, height=20) 
 botao.pack(padx=10, pady=10)
 
-resposta = customtkinter.CTkEntry(janela, placeholder_text= "resposta:", width=250, height=45)
-resposta.pack(padx=10, pady=10)
+
+resposta = CTkTextbox(master=app, scrollbar_button_hover_color='#522fdf', scrollbar_button_color="#a4e0cd", corner_radius=16, border_color="#a4e0cd", border_width=1)
+resposta.pack(padx=10, pady=5,)
 
 
-janela.mainloop()
+
+
+app.mainloop()
