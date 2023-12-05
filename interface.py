c30f834711f6b4a6cb18951aec959ccf0f08a4b7
Daniel Gomes <138718021+DanielGomesSantos@users.noreply.github.com>
2023-12-05 11:01:52 -0300

    05/12/2023

--- interface.py
+++ interface.py
@@ -4,32 +4,30 @@ import customtkinter as ct
 from PIL import Image, ImageTk
 import time
 import os
-import cProfile, random
+import cProfile 
 import webbrowser
 
-#from generate import conversa_com_chatbot
-
-root = ct.CTk()
-root.title("Assistente Virtual")
+janela = ct.CTk()
+janela.title("Assistente Virtual")
 ct.set_appearance_mode("light") #dark
 ct.set_default_color_theme=('#f6f2f3')
 bgColor = "#f6f2f3"   #black
-root.configure(fg_color=bgColor)
-root.geometry("1000x650+500+100")
+janela.configure(fg_color=bgColor)
+janela.geometry("1000x650+500+100")
 
-file_path = "imagename.txt"
+caminho_arquivo = "imagename.txt"
 
-if os.path.exists(file_path):
+if os.path.exists(caminho_arquivo):
     pass
 else:
-    with open(file_path, "x") as f:
+    with open(caminho_arquivo, "x") as f:
         pass
 
-with open(file_path, "w") as img_src:
-    image_list ='imagens\\avator.png'
-    my_image = image_list
-    print(my_image)
-    img_src.write(f"{my_image}")
+with open(caminho_arquivo, "w") as img_src:
+    lista_imagem ='imagens\\avator.png'
+    minha_imagem = lista_imagem
+    print(minha_imagem)
+    img_src.write(f"{minha_imagem}")
 
 def abrir_link():
         link = 'https://www.samsung.com/br/'
@@ -37,40 +35,43 @@ def abrir_link():
 
 
 class ChatApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.resizable(True, True)
-        self.root.geometry("520x620+500+100")
+    def __init__(self, janela):
+        self.janela = janela
+        self.janela.resizable(True, True)
+        self.janela.geometry("520x620+500+100")
 
-        self.optionFram = Frame(root, bg="#f6f2f3") #black
-        # All imagas for chatfram code
+        self.optionFram = Frame(janela, bg="#f6f2f3") 
+        
 
-        manIconSrc2 = Image.open("imagens\\samsung_avatar-removebg-preview.png")
-        manIconImage2 = ImageTk.PhotoImage(manIconSrc2.resize((40, 40)))
-        submitIconSrc = Image.open("imagens\\enviar.png")
-        submitIconImage = ImageTk.PhotoImage(submitIconSrc.resize((40, 40)))
-        messageIconSrc = Image.open("imagens\\message.png")
-        messageIconImage = ImageTk.PhotoImage(messageIconSrc.resize((40, 40)))
-        chatIconSrc = Image.open("imagens\\samsung.png")
-        chatIconImage = ImageTk.PhotoImage(chatIconSrc.resize((50, 60)))
+        man_icone_robo = Image.open("Imagens/samsung_avatar-removebg-preview.png")
+        man_imagem_robo = ImageTk.PhotoImage(man_icone_robo.resize((40, 40)))
 
-        # All imagas for chatfram code ends here
+        man_icone_enviar = Image.open("Imagens/enviar.png")
+        man_imagem_enviar = ImageTk.PhotoImage(man_icone_enviar.resize((40, 40)))
 
-        self.messageIcon = ct.CTkButton(self.optionFram, text="",image=messageIconImage, width=0, bg_color=bgColor, fg_color=bgColor)
-        self.messageIcon.pack(side=RIGHT, anchor="nw", padx=5, pady=5)
+        man_icone_mensagem = Image.open("imagens\\message.png")
+        man_imagem_mansagem = ImageTk.PhotoImage(man_icone_mensagem.resize((40, 40)))
 
-        self.manIcon2 = ct.CTkButton(self.optionFram, text="",image=manIconImage2, height=1, width=1, bg_color=bgColor, fg_color=bgColor,
+        man_icone_chat = Image.open("Imagens/samsung.png")
+        man_imagem_chat = ImageTk.PhotoImage(man_icone_chat.resize((55, 55)))
+
+       
+
+        self.menIcone = ct.CTkButton(self.optionFram, text="",image=man_imagem_mansagem, width=0, bg_color=bgColor, fg_color=bgColor)
+        self.menIcone.pack(side=RIGHT, anchor="nw", padx=5, pady=5)
+
+        self.manIcone2 = ct.CTkButton(self.optionFram, text="",image=man_imagem_robo, height=1, width=1, bg_color=bgColor, fg_color=bgColor,
                                 corner_radius=20)
-        self.manIcon2.pack(side=LEFT, anchor="ne")
+        self.manIcone2.pack(side=LEFT, anchor="ne")
 
-        self.username = ct.CTkLabel(self.optionFram, text="Fale com a SAM", font=("consolas", 20), text_color="#010100") #d8d8df
-        self.username.pack(side=LEFT, padx=5, pady=7, anchor="ne")
+        self.nome_sam = ct.CTkLabel(self.optionFram, text="Fale com a SAM", font=("consolas", 20), text_color="#010100") 
+        self.nome_sam.pack(side=LEFT, padx=5, pady=7, anchor="ne")
         self.optionFram.pack(side=TOP, fill=X, anchor="n")
 
-        self.canvas = tk.Canvas(root, bg="#f6f2f3", highlightthickness=0) #black
+        self.canvas = tk.Canvas(janela, bg="#f6f2f3", highlightthickness=0)  
 
-        self.label_frame = tk.Frame(self.canvas, bg="#f6f2f3") #black
-        self.label_frame.pack(side="left", fill="both", expand=True) 
+        self.label_frame = tk.Frame(self.canvas, bg="#f6f2f3")   
+        self.label_frame.pack(side="left", fill="both", expand=True)
 
         self.scrollable_window = self.canvas.create_window((0, 0), window=self.label_frame, anchor="nw")
 
@@ -78,39 +79,42 @@ class ChatApp:
 
         self.label_frame.bind("<Configure>", self.configure_scroll_region)
 
-        self.scrollbar = ct.CTkScrollbar(self.canvas, orientation="vertical", command=self.canvas.yview)
-        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.barra_rolar = ct.CTkScrollbar(self.canvas, orientation="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.barra_rolar.set)
         self.canvas.yview_moveto(1.0)
 
-        self.scrollbar.pack(side="right", fill=Y)
+        self.barra_rolar.pack(side="right", fill=Y)
 
         self.canvas.bind("<Configure>", self.resize_frame)
         self.canvas.pack(side=TOP, fill=BOTH)
 
-        bgColorChatFram = "#7eabde" #080420
-        self.chatFram = Frame(root, bg=bgColorChatFram)
+        bgColorChatFram = "#a8c7fc"   
+        self.chatFram = Frame(janela, bg=bgColorChatFram)
+        ########   EU PAREI AQUI:  #######################
 
-        self.chatIcon = ct.CTkButton(self.chatFram, text="",image=chatIconImage, width=20, bg_color=bgColorChatFram, fg_color=bgColorChatFram)
+
+        self.chatIcon = ct.CTkButton(self.chatFram, text="",image=man_imagem_chat, width=20, bg_color=bgColorChatFram, fg_color=bgColorChatFram)
         self.chatIcon.pack(side=LEFT, padx=5)
         self.chatIcon.configure(command=abrir_link)
-
-        self.msgInput = ct.CTkEntry(self.chatFram, placeholder_text="Digite sua pergunta aqui ...", height=40, font=("consolas", 18))
+    
+        
+        self.msgInput = ct.CTkEntry(self.chatFram, placeholder_text="Digite aqui...", height=40, text_color="#010100", font=("consolas", 18))
         self.msgInput.pack(side=LEFT, pady=10, padx=0, fill=X, expand=True)
 
-        self.submitBtn = ct.CTkButton(self.chatFram, text="", image=submitIconImage, bg_color=bgColorChatFram, fg_color=bgColorChatFram, height=40, width=20, hover_color=bgColorChatFram)
+        self.submitBtn = ct.CTkButton(self.chatFram, text="", image=man_imagem_enviar, bg_color=bgColorChatFram, fg_color=bgColorChatFram, height=40, width=20, hover_color=bgColorChatFram)
         self.submitBtn.pack(side=LEFT, padx=0, pady=8)
-        self.submitBtn.configure(command=self.sendMessage)
+        self.submitBtn.configure(command=self.enviarMensagem)
 
         self.chatFram.pack(side=BOTTOM, fill=X, ipady=5)
 
-        self.root.bind("<Return>", lambda event: self.sendMessage())
+        self.janela.bind("<Return>", lambda event: self.enviarMensagem())
 
-    def sendMessage(self):
-        with open(file_path, "r") as f:
+    def enviarMensagem(self):
+        with open(caminho_arquivo, "r") as f:
             self.imagename = f.read()
             print(self.imagename)
 
-        self.message = (self.msgInput.get())
+        self.message = self.msgInput.get()
         self.msgInput.delete(0, END)
         user_image_src = Image.open(self.imagename)
         user_image = ImageTk.PhotoImage(user_image_src.resize((40, 40)))        
@@ -121,27 +125,28 @@ class ChatApp:
             self.current_time_label = ct.CTkLabel(self.label_frame, text=self.current_time, font=("consolas", 12))
             self.current_time_label.pack(side=TOP, anchor="ne", pady=0, padx=45)
 
-            self.user_frame = ct.CTkFrame(self.label_frame, fg_color="#f6f2f3") # borda do pergunta
+            self.user_frame = ct.CTkFrame(self.label_frame, fg_color="#f6f2f3") 
             self.user_frame.pack(side=TOP, anchor="ne")
-            self.user_label = ct.CTkLabel(self.user_frame, text=self.message, font=("Poppins", 15), fg_color="#95bbc9", corner_radius=4,wraplength=250) #419f5b
-                                    
+            self.user_label = ct.CTkLabel(self.user_frame, text=self.message, font=("Poppins", 15), fg_color="#419f5b", corner_radius=4,
+                                    wraplength=250)
             self.user_label.pack(side=LEFT, anchor="nw", pady=1, ipadx=15, ipady=6, padx=10)
-            self.user_image_label = ct.CTkLabel(self.user_frame, text="", image=user_image, fg_color="#f6f2f3") #borda pergunta do icone 
+            self.user_image_label = ct.CTkLabel(self.user_frame, text="", image=user_image, fg_color="#f6f2f3") 
             self.user_image_label.pack(side=TOP, pady=13)
-            self.root.update_idletasks()
+            self.janela.update_idletasks()
             self.canvas.update_idletasks()
             self.canvas.yview_moveto(1.0)
             try:
-                #self.to_respond = conversa_com_chatbot(self.message)
+               
                 if self.to_respond:
                     self.current_time = time.strftime("%H:%M")
                     self.current_time_label = ct.CTkLabel(self.label_frame, text=self.current_time, font=("consolas", 12))
                     self.current_time_label.pack(side=TOP, anchor="nw", pady=0, padx=55)
-                    self.bot_frame = ct.CTkFrame(self.label_frame, fg_color="#f6f2f3")#black
+                    self.bot_frame = ct.CTkFrame(self.label_frame, fg_color="#f6f2f3") 
                     self.bot_frame.pack(side=TOP, anchor="nw", padx=10)
-                    self.bot_response_label = ct.CTkLabel(self.bot_frame, text=self.to_respond, font=("Poppins", 14), fg_color="#444", corner_radius=6, wraplength=300)                
+                    self.bot_response_label = ct.CTkLabel(self.bot_frame, text=self.to_respond, font=("Poppins", 14), 
+                                        fg_color="#444", corner_radius=6, wraplength=300)
                     self.bot_response_label.pack(side=RIGHT, anchor="ne", padx=10, pady=1, ipady=8, ipadx=10)
-                    self.bot_image_label = ct.CTkLabel(self.bot_frame, text="", image=bot_image, fg_color="#f6f2f3")#black
+                    self.bot_image_label = ct.CTkLabel(self.bot_frame, text="", image=bot_image, fg_color="#f6f2f3") 
                     self.bot_image_label.pack(side=TOP, pady=13)
                     self.canvas.update_idletasks()
                     self.canvas.yview_moveto(1.0)
@@ -160,7 +165,7 @@ class ChatApp:
     def resize_frame(self, e):
         self.canvas.itemconfigure(self.scrollable_window, width=e.width-30)
         
-# AvatorPage(root)
-ChatApp(root)
 
-cProfile.run('root.mainloop()')
+ChatApp(janela)
+
+cProfile.run("janela.mainloop()")
